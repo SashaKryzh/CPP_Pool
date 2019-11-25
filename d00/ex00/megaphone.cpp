c8e7a7175a2b33753eed65b837ced88751e58c33
Sasha <sashakryzh@gmail.com>
2019-11-25 14:00:31 +0200

    start

---
+++ d00/ex00/megaphone.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+
+	for (int i = 1; i < argc; i++) {
+		std::string s = argv[i];
+		std::transform(s.begin(), s.end(), s.begin(), ::toupper);
+		std::cout << s;
+	}
+
+	std::cout << std::endl;
+
+	return 0;
+}
