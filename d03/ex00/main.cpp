cfe1989cac7306d9e4f31f5da6144f3017cb0fac
Sasha <sashakryzh@gmail.com>
2019-12-02 20:13:33 +0200

    bugfix ex00

--- d03/ex00/main.cpp
+++ d03/ex00/main.cpp
@@ -1,9 +1,10 @@
 #include "FragTrap.hpp"
+#include <iostream>
 
 int main()
 {
-    FragTrap f1("First");
-    FragTrap f2("Second");
+    FragTrap f1("Frag 1");
+    FragTrap f2("Frag 2");
 
     f1.meleeAttack("Your mama");
     f2.rangedAttack("Your papa");
@@ -17,10 +18,12 @@ int main()
     f2.takeDamage(50);
     f2.beRepaired(50);
 
-    f1.beRepaired(50);
     f1.takeDamage(1000);
 
+    std::cout << "f3 = f1" << std::endl;
+
     FragTrap f3 = f1;
+    f3.beRepaired(500);
     f3.vaulthunter_dot_exe("Your grandpapa");
 
     return 0;
